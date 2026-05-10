f2eb90a6d5a31c6dbd6f9a961835e7b2e2cdc13d
Ti-m-T <tim.tellier@epfl.ch>
2026-05-10 17:21:19 +0200

    Ajout de nb_atom dans chemical

--- experiment.py
+++ experiment.py
@@ -22,18 +22,23 @@ from thermo import chemical as density_finder
 @dataclass
 class Chemical: # Class grouping all chemicals 
     smiles: str
-    mass: float=0.0
+    mass: float = 0.0
 
     def __post_init__(self):
-        self.mol=Chem.MolFromSmiles(self.smiles) # Gets molecular object from smiles
+        self.mol = Chem.MolFromSmiles(self.smiles) # Gets molecular object from smiles
         if self.mol:
-            self.mw=rdMolDescriptors.CalcExactMolWt(self.mol) # Calculates molecular weight from smiles
-            self.logp=rdMolDescriptors.SlogP_VSA_(self.mol) # Finds the hydrophobicity of the molecule
-            self.mol_f=rdMolDescriptors.CalcMolFormula(self.mol) # Finds the molecular formula using the smiles
-            self.coeff: int=1
+            self.mw = rdMolDescriptors.CalcExactMolWt(self.mol) # Calculates molecular weight from smiles
+            self.logp = rdMolDescriptors.SlogP_VSA_(self.mol) # Finds the hydrophobicity of the molecule
+            self.mol_f = rdMolDescriptors.CalcMolFormula(self.mol) # Finds the molecular formula using the smiles
+            self.coeff : int = 1
+
+
+            mol_H = Chem.AddHs(self.mol)
+
+            self.nb_atom = mol_H.GetNumAtoms()
         else:
-            self.mw=0.0
-            self.smiles="Invalid"
+            self.mw = 0.0
+            self.smiles = "Invalid"
     
 
 @dataclass
@@ -43,12 +48,12 @@ class Extras(Chemical):
 
 @dataclass    
 class LiquidChemical(Chemical): # Subclass of chemical grouping all chemicals that are liquids, mainly used for solvents and extractants
-    volume: float =0.0
-    density: float=0.0
+    volume: float = 0.0
+    density: float = 0.0
 
     def __post_init__(self):
         super().__post_init__()
-        self.density=density_finder(f"SMILES={self.smiles}").rho
+        self.density = density_finder(f"SMILES={self.smiles}").rho
 
     @property
     def m_solvent(self):
@@ -71,21 +76,25 @@ class Reaction:
     #extractants: list[Extractant] = field(default_factory=list)
     
     def stoich_of_reaction(self):
-        reac={reactant.mol_f for reactant in self.reactants}
-        prod={product.mol_f for product in self.products}
-        reactants_coeff,products_coeff=balance_stoichiometry(reac,prod)
-        for reactant in self.reactants:
-            reactant.coeff=reactants_coeff.get(reactant.mol_f,1)
-        for product in self.products:
-            product.coeff=products_coeff.get(product.mol_f,1)
+        reac = {reactant.mol_f for reactant in self.reactants}
+        prod = {product.mol_f for product in self.products}
+        reactants_coeff,products_coeff = balance_stoichiometry(reac,prod)
         
-r1=Chemical(smiles="C",mass=1)
-r2=Chemical(smiles="O=O",mass=1)
-p1=Chemical(smiles="C(=O)=O",mass=1)
-p2=Chemical(smiles="O",mass=1)
-react=Reaction([r1,r2],[p1,p2])
-react.stoich_of_reaction()
-for r in react.reactants:
-    print(r.coeff)
-for p in react.products:
-    print(p.coeff)
+        for reactant in self.reactants:
+            reactant.coeff = reactants_coeff.get(reactant.mol_f,1)
+        for product in self.products:
+            product.coeff = products_coeff.get(product.mol_f,1)
+        
+#r1 = Chemical(smiles="C",mass=1)
+#r2 = Chemical(smiles="O=O",mass=1)
+#p1 = Chemical(smiles="C(=O)=O",mass=1)
+#p2 = Chemical(smiles="O",mass=1)
+#react = Reaction([r1,r2],[p1,p2])
+#react.stoich_of_reaction()
+#for r in react.reactants:
+#    print(r.coeff) 
+#for p in react.products:
+#    print(p.coeff)
+
+mol = Chemical("CC=O")
+print(mol.nb_atom)
